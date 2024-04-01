5a80b141776a880498e1dce164975a3490acb0d5
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-04-01 12:28:48 -0400

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+
+if(x < 0  || Number.isInteger(x)==false){
+    return "ERROR"
+}
+
+if (y < 0 || Number.isInteger(y)==false){
+    return "ERROR"
+}
+
+let sum = 0;
+if(x<y){
+
+    for(i = x ; i<=y ; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+if(y<x){
+
+    for(i = y ; i<=x ; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+
+
 
 };
 

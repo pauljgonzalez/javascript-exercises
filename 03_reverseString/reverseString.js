02eeba4f34ed5caf359dade36551717bed78adae
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-04-01 11:51:22 -0400

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let word = "";
+    let stringCount = string.length;
+    for( i=stringCount-1 ; i>=0 ; i--){
+        word += string[i];
+    }    
+    return word;
 
 };
 
